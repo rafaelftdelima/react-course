83f88abd89c32e372460c569d6873da14367b3ff
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-05-10 08:11:52 -0300

    0303 useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
 import React from 'react';
 
-import Product from './Product';
-
 const App = () => {
-  const [product, setProduct] = React.useState(null);
+  const [cart, setCart] = React.useState(0);
+  const [notification, setNotification] = React.useState(null);
+  const timeoutRef = React.useRef();
 
-  React.useEffect(() => {
-    const preferencial_product = localStorage.getItem('preferencial_product');
+  const handleClick = () => {
+    setCart(cart + 1);
+    setNotification('Item adicionado ao carrinho');
 
-    if (preferencial_product)
-      setProduct(preferencial_product);
-  }, []);
-
-  React.useEffect(() => {
-    if (product !== null)
-      localStorage.setItem('preferencial_product', product);
-  }, [product]);
-
-  const handleClick = ({ target }) => {
-    const product_name = target.innerText.toLowerCase();
-
-    setProduct(product_name);
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => {
+      setNotification(null);
+    }, 2000);
   };
 
   return (
     <div>
-      <h1>Preferência</h1>
-      <button onClick={handleClick} style={{marginRight: '1rem'}}>Notebook</button>
-      <button onClick={handleClick}>Smartphone</button>
-
-      <Product product_name={product} />
+      <p>{notification}</p>
+      <button onClick={handleClick}>Adicionar novo item ao carrinho</button>
+      <p>Número de itens: {cart}</p>
     </div>
   );
 };
