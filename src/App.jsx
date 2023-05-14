d3f930b0b3dacbe0c6664050d606442c732b1afe
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-05-14 10:28:35 -0300

    0412 Desafio Formulários

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,86 @@
 import React from 'react';
-import useForm from './Hooks/useForm';
+import Radio from './Form/Radio';
 
-import Input from './Input';
+const questions = [
+  {
+    question: 'Qual método é utilizado para criar componentes?',
+    options: [
+      'React.makeComponent()',
+      'React.createComponent()',
+      'React.createElement()',
+    ],
+    answer: 'React.createElement()',
+    id: 'p1',
+  },
+  {
+    question: 'Como importamos um componente externo?',
+    options: [
+      'import Component from "./Component"',
+      'require("./Component")',
+      'import "./Component"',
+    ],
+    answer: 'import Component from "./Component"',
+    id: 'p2',
+  },
+  {
+    question: 'Qual hook não é nativo?',
+    options: ['useEffect()', 'useFetch()', 'useCallback()'],
+    answer: 'useFetch()',
+    id: 'p3',
+  },
+  {
+    question: 'Qual palavra deve ser utilizada para criarmos um hook?',
+    options: ['set', 'get', 'use'],
+    answer: 'use',
+    id: 'p4',
+  },
+];
 
 const App = () => {
-  const cep = useForm('cep');
-  const email = useForm('email');
-  const nome = useForm();
-  const sobrenome = useForm(false);
+  const [answers, setAnswers] = React.useState({
+    p1: '',
+    p2: '',
+    p3: '',
+    p4: '',
+  });
+  const [slide, setSlide] = React.useState(0);
+  const [result, setResult] = React.useState(null);
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  function handleChange({ target }) {
+    setAnswers({ ...answers, [target.id]: target.value })
+  }
 
-    if (cep.validate() && email.validate() && nome.validate())
-      console.log('Enviado');
-    else
-      console.log('Não enviado');
+  function finalResult() {
+    const rightAnswers = questions.filter(
+      ({ id, answer }) => answers[id] === answer,
+    );
+
+    setResult(`Você acertou ${rightAnswers.length} de ${questions.length} perguntas.`);
+  }
+
+  function handleClick() {
+    setSlide(slide + 1);
+
+    if (slide == questions.length - 1)
+      finalResult();
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Input id='nome' label='Nome' type='text' {...nome} />
-      <Input id='sobrenome' label='Sobrenome' type='text' {...sobrenome} />
-      <Input id='email' label='E-mail' type='email' {...email} />
-      <Input id='cep' label='CEP' type='text' placeholder='00000-000' {...cep} />
-      <button>Enviar</button>
+    <form onSubmit={(e) => e.preventDefault()}>
+      {questions.map((question, index) => (
+        <Radio 
+          key={question.id} 
+          active={slide === index} 
+          value={answers[question.id]} 
+          onChange={handleChange} 
+          {...question} 
+        />
+      ))}
+      {result ? (
+        <p>{result}</p>
+      ) : (
+        <button onClick={handleClick}>Próxima</button>
+      )}
     </form>
   );
 };
